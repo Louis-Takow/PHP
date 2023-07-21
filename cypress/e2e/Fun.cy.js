bec2d25e5ec39792ced4c8d424b0bf7cba2f795c
unknown <louistt1995@gmail.com>
2023-07-21 13:33:16 +0100

    2nd commit

--- cypress/e2e/Fun.cy.js
+++ cypress/e2e/Fun.cy.js
@@ -17,7 +17,7 @@ describe('OrangeHRM', () => {
  
   // it('Admin',()=>{
   //   cy.visit('');
-  //   cy.Orangelogin('Admin','admin123');
+  //   cy.Orangelgin('Admin','admin123');
 
   //   examplePage.getsidePanel().contains('Admin').click();
   //   examplePage.getpageHeading()
